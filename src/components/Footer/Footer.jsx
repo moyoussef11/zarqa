9cc2f21db5ffac24411a32a499c020417148e501
moyoussef11 <mohmedyoussef472@gmail.com>
2024-06-02 13:56:20 +0300

    q

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -15,7 +15,7 @@ const Footer = () => {
     <footer className="mt-auto w-full text-center sm:text-start text-white p-5 bg-[#2B9DD9]">
       <div className="container mx-auto flex flex-wrap justify-between">
         <div className="w-full sm:w-1/2 md:w-1/4 lg:w-1/6 px-4 mb-4">
-          <h2 className="font-bold mb-4 text-7xl bg-white mainColor w-fit px-3 flex items-center justify-center mx-auto sm:mx-0 rounded ">Q</h2>
+          <h2 className="font-bold mb-4 text-7xl bg-white mainColor w-fit px-3 py-0 flex items-center justify-center mx-auto sm:mx-0 rounded ">Q</h2>
           <ul>
             <li className="font-bold">UNITED</li>
             <li className="font-bold">ZARQA</li>
